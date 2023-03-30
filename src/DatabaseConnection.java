c186a0495167c19c6be03cdf9807aa2a05074d5b
Miquel Albos Campoy <miquel.albos@students.salle.url.edu>
2023-03-30 11:24:09 +0200

    Database Connection

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,4 +1,4 @@
-import java.sql.Connection;
+import java.sql.*;
 
 public class DatabaseConnection {
 
@@ -15,5 +15,60 @@ public class DatabaseConnection {
         this.nameUser = nameUser;
         this.password = password;
         this.url = "jdbc:mysql://"+ip+":"+port+"/"+databaseName;
+    }
+    private void inicialitzeJSON(Configuration configuration){
+        nameUser = configuration.getUser();
+        password = configuration.getPassword();
+        port = configuration.getPort();
+        server = configuration.getServer();
+    }
+    private void connection(){
+        try {
+            connection = DriverManager.getConnection(url,nameUser,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private DatabaseConnection getDatabaseConnection (){
+        if(databaseConnection == null){
+            databaseConnection = new DatabaseConnection(nameUser,password,server,port,"BetweenUs");
+            databaseConnection.connection();
+        }
+        return databaseConnection;
+    }
+
+    private void insert(String sqlQuery){
+        try{
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlQuery);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private ResultSet select(String sqlSelect){
+        ResultSet resultSet=null;
+        try{
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(sqlSelect);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return resultSet;
+    }
+    private void connect(String sqlConnect){
+        try{
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlConnect);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void update(String sqlUpdate){
+        try{
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlUpdate);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
