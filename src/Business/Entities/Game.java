6305503273fe30f978c9f35b38577b86afe977f1
Pol Montpart Falip <pol.montpart@students.salle.url.edu>
2023-05-09 13:07:22 +0200

    Game Logic

---
+++ src/Business/Entities/Game.java
@@ -0,0 +1,25 @@
+package Business.Entities;
+
+public class Game {
+
+    String name;
+    int N_persponajes;
+    int N_impostores;
+    String mapa;
+
+    public Game(String name, int N_persponajes, int N_impostores, String mapa) {
+        this.name = name;
+        this.N_persponajes = N_persponajes;
+        this.N_impostores = N_impostores;
+        this.mapa = mapa;
+    }
+
+    public String getName(){return this.name;}
+    public void setName(String name){this.name = name;}
+    public int getN_persponajes(){return this.N_persponajes;}
+    public void setN_persponajes(int N_persponajes){this.N_persponajes = N_persponajes;}
+    public int getN_impostores() {return this.N_impostores;}
+    public void setN_impostores(int N_impostores){this.N_impostores = N_impostores;}
+    public String getMapa(){return this.mapa;}
+    public void setMapa(String mapa){this.mapa = mapa;}
+}
---
+++ src/Business/GameManager.java
@@ -0,0 +1,47 @@
+package Business;
+
+import Business.Entities.Game;
+import Business.Entities.User;
+import persistence.GameDAO;
+
+public class GameManager {
+    private String name_game;
+    private int N_persponajes;
+    private int N_impostores;
+    private String mapa;
+    private String name_user;
+    private String email;
+    private String password;
+    public GameManager(String name_game, int N_persponajes, int N_impostores, String mapa, String name_user, String email, String password){
+        this.name_game = name_game;
+        this.N_persponajes = N_persponajes;
+        this.N_impostores = N_impostores;
+        this.mapa = mapa;
+        this.name_user = name_user;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void createGame(String name_game, int N_persponajes, int N_impostores, String mapa, String name_user, String email, String password, String color){
+        GameDAO gameDAO = new GameDAO();
+        Game game = new Game(name_game, N_persponajes, N_impostores, mapa);
+        User user = new User(name_user, email, password);
+
+        gameDAO.gameRecord(game, user, color);
+    }
+
+    public void inicarGame(String name_game, int N_persponajes, int N_impostores, String mapa){
+        GameDAO gameDAO = new GameDAO();
+        Game game = new Game(name_game, N_persponajes, N_impostores, mapa);
+
+        gameDAO.startGame(game);
+    }
+    /*
+    public void createUser (String name, String email, String password){
+        UserDAO userDAO = new UserDAO();
+        User user = new User(name,email,password);
+
+        userDAO.signupUser(user);
+    }
+     */
+}
